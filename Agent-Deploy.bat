126fcb891d0546f6061b4db2a3fe7488ab37bf47
DaNapsta <bayates826@gmail.com>
2024-01-02 15:05:49 -0600

    new file:   Agent-Deploy.ps1

---
+++ Agent-Deploy.bat
@@ -0,0 +1,20 @@
+@echo off
+
+:: set filename for temporary PS script
+set PsScript=%~dpn0.ps1
+
+:: create the PS Script
+echo function DownloadScript($url, $zipname, $folderName){ >> %PsScript%
+echo    $desktop = [Environment]::GetFolderPath("Desktop") >> %PsScript%
+echo    Invoke-WebRequest -Uri $url -OutFile "$desktop\$zipname" >> %PsScript%
+echo    Add-Type -AssemblyName System.IO.Compression.FileSystem >> %PsScript%
+echo    [System.IO.Compression.ZipFile]::ExtractToDirectory("$desktop\$zipName", "$desktop") >> %PSScript%
+echo    Remove-Item "$desktop\$zipname" >> %PsScript%
+echo    Get-ChildItem -Path "$desktop\$foldername" ^| Move-Item -Destination $desktop >> %PsScript%
+echo    Remove-Item "$desktop\$foldername" -recurse -force >> %PsScript%
+echo } >> %PsScript%
+
+echo DownloadScript "https://github.com/danapsta/Arrow-Deploy/archive/refs/heads/main.zip" "Deploy.zip" "Deploy-main" >> %psScript%
+echo Start-Process -FilePath $desktop\$foldername\Agent.exe /quiet -Verb RunAs -Wait
+
+powershell -executionpolicy bypass -file %PsScript%
