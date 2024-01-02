9b7068e05b4cbc99bc9ed95a1135681bc3a6111b
DaNapsta <bayates826@gmail.com>
2024-01-02 15:15:02 -0600

    modified:   Agent-Deploy.bat

--- Agent-Deploy.bat
+++ Agent-Deploy.bat
@@ -4,17 +4,19 @@
 set PsScript=%~dpn0.ps1
 
 :: create the PS Script
-echo function DownloadScript($url, $zipname, $folderName){ >> %PsScript%
-echo    $desktop = [Environment]::GetFolderPath("Desktop") >> %PsScript%
-echo    Invoke-WebRequest -Uri $url -OutFile "$desktop\$zipname" >> %PsScript%
-echo    Add-Type -AssemblyName System.IO.Compression.FileSystem >> %PsScript%
-echo    [System.IO.Compression.ZipFile]::ExtractToDirectory("$desktop\$zipName", "$desktop") >> %PSScript%
-echo    Remove-Item "$desktop\$zipname" >> %PsScript%
-echo    Get-ChildItem -Path "$desktop\$foldername" ^| Move-Item -Destination $desktop >> %PsScript%
-echo    Remove-Item "$desktop\$foldername" -recurse -force >> %PsScript%
-echo } >> %PsScript%
-
-echo DownloadScript "https://github.com/danapsta/Arrow-Deploy/archive/refs/heads/main.zip" "Deploy.zip" "Deploy-main" >> %PsScript%
-echo Start-Process -FilePath $desktop\$foldername\Agent.exe /quiet -Verb RunAs -Wait >> %PsScript%
+(
+echo function DownloadScript($url, $zipname, $folderName){
+echo    $desktop = [Environment]::GetFolderPath("Desktop")
+echo    Invoke-WebRequest -Uri $url -OutFile "$desktop\$zipname"
+echo    Add-Type -AssemblyName System.IO.Compression.FileSystem
+echo    [System.IO.Compression.ZipFile]::ExtractToDirectory("$desktop\$zipname", "$desktop")
+echo    Remove-Item "$desktop\$zipname"
+echo    Get-ChildItem -Path "$desktop\$folderName" ^| Move-Item -Destination $desktop
+echo    Remove-Item "$desktop\$folderName" -recurse -force
+echo }
+echo $desktop = [Environment]::GetFolderPath("Desktop")
+echo DownloadScript "https://github.com/danapsta/Arrow-Deploy/archive/refs/heads/main.zip" "Deploy.zip" "Deploy-main"
+echo Start-Process -FilePath "$desktop\Deploy-main\Agent.exe" -ArgumentList '/quiet' -Verb RunAs -Wait
+) > %PsScript%
 
 powershell -executionpolicy bypass -file %PsScript%
