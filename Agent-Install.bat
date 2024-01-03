e6e04c607d9f411f2ba4956884a5269672104ae6
Danapsta <toastyanswers@gmail.com>
2024-01-03 00:24:57 -0600

    modified:   Agent-Install.bat

--- Agent-Install.bat
+++ Agent-Install.bat
@@ -4,8 +4,14 @@
 set PsScript=%~dpn0.ps1
 set BaScript=%~dpn0.bat
 
+:: Create Temp Directory
+echo $directoryPath = "C:\Temp" > %PsScript%
+echo if (-not (Test-Path $directoryPath)) { >> %PsScript%
+echo     new-item -path $directoryPath -ItemType Directory >> %PsScript%
+echo } >> %PsScript%
+
 :: Generate Admin Credential
-echo $username = "admin" > %PsScript%
+echo $username = "admin" >> %PsScript%
 echo $password = "fCC1nC" ^| ConvertTo-SecureString -AsPlainText -Force >> %PsScript%
 echo $password ^| ConvertFrom-SecureString ^| Out-File "C:\Temp\Secret.txt" >> %PsScript%
 
